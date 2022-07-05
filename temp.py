d95d564ca0359f1fa1e77ab43ed1d1dd210566fe
Shi Chen <shichen.py@gmail.com>
2022-07-05 12:47:41 +0900

    feat: added bottom-up pose estimation.

--- temp.py
+++ temp.py
@@ -6,7 +6,7 @@ import warnings
 import torch
 
 from mmdet.apis import inference_detector, init_detector
-from mmpose.apis import (inference_top_down_pose_model,
+from mmpose.apis import (inference_top_down_pose_model, inference_bottom_up_pose_model,
                          init_pose_model, process_mmdet_results)
 from mmpose.datasets import DatasetInfo
 from utils import display_results, generate_obj_colors
@@ -31,6 +31,11 @@ def parse_args():
     parser.add_argument(
         '--kpt-thr', type=float, default=0.3, help='Keypoint score threshold')
     parser.add_argument(
+        '--pose-nms-thr',
+        type=float,
+        default=0.9,
+        help='OKS threshold for pose NMS')
+    parser.add_argument(
         '--radius',
         type=int,
         default=4,
@@ -48,14 +53,14 @@ def main():
     args = parse_args()
 
     print('Initializing model...')
-    # build the detection model from a config file and a checkpoint file
-    det_model = init_detector(
-        args.det_config, args.det_checkpoint, device=args.device.lower())
+    # build the detection model from a config file and a checkpoint file.
+    det_model = init_detector(args.det_config, args.det_checkpoint, device=args.device.lower())
+
+    # get object bounding box colors.
     obj_colors = generate_obj_colors(det_model.CLASSES)
 
-    # build the pose model from a config file and a checkpoint file
-    pose_model = init_pose_model(
-        args.pose_config, args.pose_checkpoint, device=args.device.lower())
+    # build the pose model from a config file and a checkpoint file.
+    pose_model = init_pose_model(args.pose_config, args.pose_checkpoint, device=args.device.lower())
 
     dataset = pose_model.cfg.data['test']['type']
     # get datasetinfo
@@ -83,36 +88,46 @@ def main():
         ret_val, img = camera.read()
 
         # get the detection results of current frame
-        # the resulting box is (x1, y1, x2, y2)
+        # the resulting box is (x1, y1, x2, y2).
         mmdet_results = inference_detector(det_model, img)
 
-        # keep the person class bounding boxes.
-        person_results = process_mmdet_results(mmdet_results, args.det_cat_id)
+        if dataset == 'TopDownCocoDataset':
+            # keep the person class bounding boxes.
+            person_results = process_mmdet_results(mmdet_results, args.det_cat_id)
 
-        # test a single image, with a list of bboxes.
-        pose_results, returned_outputs = inference_top_down_pose_model(
-            pose_model,
-            img,
-            person_results,
-            bbox_thr=args.bbox_thr,
-            format='xyxy',
-            dataset=dataset,
-            dataset_info=dataset_info,
-            return_heatmap=return_heatmap,
-            outputs=output_layer_names)
-
-        
+            # test a single image, with a list of bboxes.
+            pose_results, returned_outputs = inference_top_down_pose_model(
+                pose_model,
+                img,
+                person_results,
+                bbox_thr=args.bbox_thr,
+                format='xyxy',
+                dataset=dataset,
+                dataset_info=dataset_info,
+                return_heatmap=return_heatmap,
+                outputs=output_layer_names)
+        elif dataset == 'BottomUpCocoDataset':
+            # test a single image.
+            pose_results, returned_outputs = inference_bottom_up_pose_model(pose_model,
+                                                                            img,
+                                                                            dataset=dataset,
+                                                                            dataset_info=dataset_info,
+                                                                            pose_nms_thr=args.pose_nms_thr,
+                                                                            return_heatmap=return_heatmap,
+                                                                            outputs=output_layer_names)           
+            
         frame = display_results(img,
                                 mmdet_results,
                                 pose_results,
-                                det_score_thr=0.5,
-                                pose_score_thr=0.3,
-                                dataset='TopDownCocoDataset',
+                                bbox_thr=args.bbox_thr,
+                                kpt_thr=args.kpt_thr,
+                                dataset=dataset,
                                 dataset_info=None,
                                 obj_colors=obj_colors,
                                 obj_class_names=det_model.CLASSES,
                                 text_color='blue',
-                                thickness=2)
+                                radius=args.radius,
+                                thickness=args.thickness)
         
         ch = cv2.waitKey(1)
         if ch == 27 or ch == ord('q') or ch == ord('Q'):
@@ -127,7 +142,6 @@ def main():
     
     camera.release()
     cv2.destroyAllWindows()
-
 
 if __name__ == '__main__':
     main()
